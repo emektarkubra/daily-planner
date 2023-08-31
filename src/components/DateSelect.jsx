b8f282ff3b5b4f00779485fc75563e10b5a46d05
Kübra Emektar <emektarrkubra@gmail.com>
2023-08-31 12:11:02 +0300

    added select-component

---
+++ src/components/DateSelect.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { DateContext } from "./context/DateContext";
+import { StyledDateSelect } from "./styled/DateSelect.styled";
+
+export default function DateSelect() {
+  const { handleGoBack, handleGoForward, month, year } = useContext(DateContext);
+
+  return (
+    <>
+      <StyledDateSelect>
+        <div></div>
+        <div></div>
+        <button onClick={handleGoBack}>{"<"}</button>
+        <div>
+          {month} {year}
+        </div>
+        <button onClick={handleGoForward}>{">"}</button>
+        <div></div>
+        <div></div>
+      </StyledDateSelect> 
+    </>
+  );
+}
---
+++ src/components/styled/DateSelect.styled.jsx
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+export const StyledDateSelect = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto auto auto auto;
+  text-align: center;
+  font-size: 1.2vw;
+`;
